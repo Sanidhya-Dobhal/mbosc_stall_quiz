1eaafa332a04e7d1c3bde24a22bb9518d92741ac
EvilGenius1010 <120200220+EvilGenius1010@users.noreply.github.com>
2024-04-06 00:52:53 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,108 @@
+let questions = [
+    {
+        "questions": "Multiple Choice: The mascot of the Linux kernel is a:",
+        "options": {
+            "a": "Penguin",
+            "b": "Robot",
+            "c": "Dog",
+            "d": "Dragon"
+        },
+        "answer": "a"
+    },
+    {
+        "questions": "True or False: The \"golang\" command is used to compile Go programs.",
+        "options": {
+            "True": True,
+            "False": False
+        },
+        "answer": "False"
+    },
+    {
+        "questions": "Multiple Choice: A popular Linux distribution known for its user-friendliness and focus on beginners is:",
+        "options": {
+            "a": "Arch Linux",
+            "b": "Ubuntu",
+            "c": "Gentoo",
+            "d": "Fedora"
+        },
+        "answer": "b"
+    },
+    {
+        "questions": "What language is Go written in?",
+        "options": {
+            "a": "Go and C#",
+            "b": "C",
+            "c": "C and Go",
+            "d": "Rust"
+        },
+        "answer": "c"
+    },
+    {
+        "questions": "Multiple Choice: Blender is a free and open-source software primarily used for what purpose?",
+        "options": {
+            "a": "Video editing",
+            "b": "3D modeling and animation",
+            "c": "Word processing",
+            "d": "Web development"
+        },
+        "answer": "b"
+    },
+    {
+        "questions": "Multiple Choice: LibreOffice is a fork (a variation) of which open-source office suite project?",
+        "options": {
+            "a": "Apache OpenOffice",
+            "b": "Microsoft Office",
+            "c": "Google Docs",
+            "d": "WPS Office"
+        },
+        "answer": "a"
+    },
+    {
+        "questions": "Approximately how many distros of Linux exist right now?",
+        "options": {
+            "a": "50",
+            "b": "100",
+            "c": "2500",
+            "d": "600"
+        },
+        "answer": "d"
+    },
+    {
+        "questions": "Multiple Choice: Android, the world's most popular mobile operating system, is based on the open-source Linux kernel. True or False?",
+        "options": {
+            "True": True,
+            "False": False
+        },
+        "answer": "True"
+    },
+    {
+        "questions": "Multiple Choice: Git, a popular version control system, is most known for allowing developers to:",
+        "options": {
+            "a": "Write and edit code collaboratively",
+            "b": "Browse the internet anonymously",
+            "c": "Design user interfaces",
+            "d": "Securely transfer large files"
+        },
+        "answer": "a"
+    },
+    {
+        "questions": "Multiple Choice: The document editor LibreOffice Writer can open and edit documents created in which popular file format?",
+        "options": {
+            "a": "Pages (.pages)",
+            "b": "DOCX",
+            "c": "ODT",
+            "d": "XLSX"
+        },
+        "answer": "c"
+    },
+    {
+        "questions": "Which of the following is NOT an open-source software?",
+        "options": {
+            "a": "MySQL",
+            "b": "Photoshop",
+            "c": "Linux",
+            "d": "VLC media player"
+        },
+        "answer": "b"
+    }
+]
